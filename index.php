3168e46acd638c4c45620cea916f65ed61644a20
Lucas Ferreira <155773773+LuksDevs@users.noreply.github.com>
2025-05-04 17:42:47 -0300

    tela de login

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/style.css">
+    <title>Tech Help Desk</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="src/css/style.css" class="logo">
+                <img src="src/svg/logo.svg" alt="Tech Help Desk" width="40">
+                <h1>Tech Help Desk</h1>
+            </a>
+        </nav>
+    </header>
+    <main id="login">
+        <div class="container-login">
+            <div class="header-nav">
+                <h3>Tech Help Desk</h3>
+            </div>
+            <form action="" method="">
+                <div class="">
+                    <input type="email" name="email" id="email" placeholder="E-mail">
+                </div>
+                <div>
+                    <input type="password" name="password" id="password" placeholder="Senha">
+                </div>
+                <button type="submit" id="btn_login">Entrar</button>
+            </form>
+        </div>
+    </main>
+</body>
+</html>
