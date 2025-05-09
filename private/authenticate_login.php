f84400fc96f4e98dd747f558fd78ac759adbf7fc
Lucas Ferreira <155773773+LuksDevs@users.noreply.github.com>
2025-05-09 18:46:12 -0300

    .

--- private/authenticate_login.php
+++ private/authenticate_login.php
@@ -1,34 +1,33 @@
-<?php 
-    session_start();
+<?php
+session_start();
 
-    $authenticate = false;
-    $user_id = null;
-    $permission_id = null;
+$authenticate = false;
+$user_id = null;
+$permission_id = null;
 
-    $permission = ['admin' => 1, 'user' => 2];
-    $users_app = [
-        ['id' => 1, 'email' => 'adm@email.com', 'password' => '1234', 'permission_id' => 1],
-        ['id' => 2, 'email' => 'LuksDevs@email.com.br', 'password' => '12345', 'permission_id' => 1],
-        ['id' => 6, 'email' => 'user01@email.com', 'password' => '1234', 'permission_id' => 2],
-        ['id' => 7, 'email' => 'user02@email.com', 'password' => '1234', 'permission_id' => 2],
-        ['id' => 8, 'email' => 'user03@email.com', 'password' => '1234', 'permission_id' => 2]       
-    ];
+$permission = ['admin' => 1, 'user' => 2];
+$users_app = [
+   ['id' => 1, 'email' => 'adm@email.com', 'password' => 'helpdeskadm', 'permission_id' => 1],
+   ['id' => 2, 'email' => 'LuksDevs@email.com', 'password' => 'helpdeskadm', 'permission_id' => 1],
+   ['id' => 6, 'email' => 'user01@email.com', 'password' => 'helpdeskuser', 'permission_id' => 2],
+   ['id' => 7, 'email' => 'user02@email.com', 'password' => 'helpdeskuser', 'permission_id' => 2],
+   ['id' => 8, 'email' => 'user03@email.com', 'password' => 'helpdeskuser', 'permission_id' => 2]
+];
 
-    foreach($users_app as $users) {
-        if($_POST['email'] == $users['email'] && $_POST['password'] == $users['password']){
-            $authenticate = true;
-            $user_id = $users['id'];
-            $permission_id = $users['permission_id'];
-        }
-    }
+foreach ($users_app as $users) {
+   if ($_POST['email'] == $users['email'] && $_POST['password'] == $users['password']) {
+      $authenticate = true;
+      $user_id = $users['id'];
+      $permission_id = $users['permission_id'];
+   }
+}
 
-    if($authenticate) {
-        $_SESSION['session_authenticate'] = 'YES';
-        $_SESSION['user_id'] = $user_id;
-        $_SESSION['permission_id'] = $permission_id;
-        header('Location: ../php/home.php');
-    } else {
-        $_SESSION['session_authenticate'] = 'NO';
-        header('Location: ../index.php?login=error_authenticate');
-    }
-?>
+if ($authenticate) {
+   $_SESSION['session_authenticate'] = 'YES';
+   $_SESSION['user_id'] = $user_id;
+   $_SESSION['permission_id'] = $permission_id;
+   header('Location: ../php/home.php');
+} else {
+   $_SESSION['session_authenticate'] = 'NO';
+   header('Location: ../index.php?login=error_authenticate');
+}
