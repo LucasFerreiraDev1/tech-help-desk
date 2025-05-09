6d932aa75e348ef8d2c771f2473072873debfa2d
Lucas Ferreira <155773773+LuksDevs@users.noreply.github.com>
2025-05-09 15:54:24 -0300

    atualização de usuários

--- private/authenticate_login.php
+++ private/authenticate_login.php
@@ -11,9 +11,7 @@
         ['id' => 2, 'email' => 'LuksDevs@email.com.br', 'password' => '12345', 'permission_id' => 1],
         ['id' => 6, 'email' => 'user01@email.com', 'password' => '1234', 'permission_id' => 2],
         ['id' => 7, 'email' => 'user02@email.com', 'password' => '1234', 'permission_id' => 2],
-        ['id' => 8, 'email' => 'user03@email.com', 'password' => '1234', 'permission_id' => 2],
-        ['id' => 9, 'email' => 'user04@email.com', 'password' => '1234', 'permission_id' => 2],
-
+        ['id' => 8, 'email' => 'user03@email.com', 'password' => '1234', 'permission_id' => 2]       
     ];
 
     foreach($users_app as $users) {
